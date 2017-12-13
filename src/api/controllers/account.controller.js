66464e92c2778b4a77ced5bac6b42bc1180d1e35
Dave Atchley <david.m.atchley@gmail.com>
2017-12-13 08:53:40 -0600

    Feature/faucet account (#9)
    * feat: added initial swag at implementing creation of new accounts via the faucet.
    
    * fix: corrected error responses, mail configuration
    
    - corrected error response if no notify address is available from the configuration
    - updated env/vars configuration to allow default along with test/prod smtp setups

--- src/api/controllers/account.controller.js
+++ src/api/controllers/account.controller.js
@@ -1,8 +1,11 @@
 const httpStatus = require('http-status');
-const { omit } = require('lodash');
+const APIError = require('../utils/APIError');
 const aqp = require('api-query-params');
+const fetch = require('isomorphic-fetch');
+const { mail } = require('../utils/email');
 const Account = require('../models/account.model');
 const { handler: errorHandler } = require('../middlewares/error');
+const { eosd, faucet } = require('../../config/vars');
 
 /**
  * Load EOS account and append to req.
@@ -35,6 +38,68 @@ exports.list = async (req, res, next) => {
     const accounts = await Account.list(query);
     const transformedAccounts = accounts.map(acct => acct.transform());
     res.json(transformedAccounts);
+  } catch (error) {
+    next(error);
+  }
+};
+
+/* eslint-disable camelcase */
+exports.createFromFaucet = async (req, res, next) => {
+  try {
+    const { name, email, wants_tokens, keys } = req.body;
+
+    const resp = await fetch(`${eosd.uri}/v1/faucet/create_account`, {
+      method: 'POST',
+      body: JSON.stringify({
+        account: name,
+        keys: {
+          owner: keys.owner,
+          active: keys.active,
+        },
+      }),
+    });
+
+    let json;
+    if (resp.headers.get('content-type').indexOf('json') > -1) {
+      json = await resp.json();
+    } else {
+      const text = await resp.text();
+      json = JSON.parse(text);
+    }
+
+    if (resp.ok && wants_tokens) {
+      if (!faucet.notify) {
+        throw new APIError({
+          status: httpStatus.BAD_REQUEST,
+          message:
+            'Unable to notify the network administrator about your request for tokens. No notify email was configured',
+        });
+      }
+
+      mail({
+        to: faucet.notify,
+        subject: `[Faucet] Request from New Account ${name}`,
+        message: `
+          <h3>The account ${name} is interested in tokens</h3>
+          <p>
+            The newly created EOS account (via faucet) has expressed an interest in acquiring test
+            tokens for development or other purposes. Here is their information:
+          </p>
+          <ul>
+            <li> <b>account:</b> ${name}</li>
+            <li> <b>email:</b> <a href="mailto:${email}">${email}</a></li>
+            <li> <b>Public Keys</b>:
+              <ul>
+                <li> <b>owner:</b> ${keys.owner}</li>
+                <li> <b>active:</b> ${keys.active}</li>
+              </ul>
+            </li>
+          </ul>
+        `,
+      });
+    }
+    res.status(resp.status);
+    res.json(json);
   } catch (error) {
     next(error);
   }
--- src/config/vars.js
+++ src/config/vars.js
@@ -6,6 +6,41 @@ require('dotenv-safe').load({
   sample: path.join(__dirname, '../../.env.example'),
 });
 
+// SMTP mail setup
+// Default to ehtereal to keep from breaking things, which
+// will just send emails to the void
+let mail = {
+  host: 'smtp.ethereal.email',
+  port: 587,
+  secure: false,
+  auth: {
+    user: 'qxyqzjzc6w5corhw@ethereal.email',
+    pass: 'Yc3SPsMfD1Dakzpw2S',
+  },
+};
+
+if (process.env.NODE_ENV === 'test' && process.env.TEST_SMTP_HOST) {
+  mail = {
+    host: process.env.TEST_SMTP_HOST,
+    port: process.env.TEST_SMTP_PORT || 587,
+    secure: false,
+    auth: {
+      user: process.env.TEST_SMTP_USER,
+      pass: process.env.TEST_SMTP_PASS,
+    },
+  };
+} else if (process.env.SMTP_HOST) {
+  mail = {
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT || 587,
+    secure: false,
+    auth: {
+      user: process.env.SMTP_USER,
+      pass: process.env.SMTP_PASS,
+    },
+  };
+}
+
 module.exports = {
   env: process.env.NODE_ENV,
   port: process.env.PORT,
@@ -14,17 +49,15 @@ module.exports = {
   mongo: {
     uri: process.env.NODE_ENV === 'test' ? process.env.MONGO_URI_TESTS : process.env.MONGO_URI,
   },
-  mail: {
-    host: 'smtp.ethereal.email',
-    port: 587,
-    secure: false,
-    auth: {
-      user: 'imqoag3vwp5pnzx7@ethereal.email',
-      pass: 'E8nFghPzFh5jbyMFS6',
-    },
-  },
+  mail,
   eosd: {
-    uri: process.env.NODE_ENV === 'test' ? process.env.EOSD_CONNECTOR_TEST_URI : process.env.EOSD_CONNECTOR_URI
+    uri:
+      process.env.NODE_ENV === 'test'
+        ? process.env.EOSD_CONNECTOR_TEST_URI
+        : process.env.EOSD_CONNECTOR_URI,
+  },
+  faucet: {
+    notify: process.env.FAUCET_NOTIFY,
   },
   logs: process.env.NODE_ENV === 'production' ? 'combined' : 'dev',
 };
